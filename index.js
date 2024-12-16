8fe57924e6322ea027f38a855ebe56ed9784f036
Sreenath <sreenathsrp256@gmail.com>
2024-12-16 18:52:00 +0530

    Cors removed

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Setting up cors
-const allowedOrigins = ['https://mtechsystem.netlify.app/'];
-const corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+// const allowedOrigins = ['https://mtechsystem.netlify.app/'];
+// const corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
   
-};
+// };
 
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 
 app.use(express.json());
