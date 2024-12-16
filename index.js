17717c671e8bd3acf44156d74e78192f626e939f
Sreenath <sreenathsrp256@gmail.com>
2024-12-16 11:12:11 +0530

    cors removed

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Setting up cors
-const allowedOrigins = [process.env.CLIENT_URL];
-const corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+// const allowedOrigins = [process.env.CLIENT_URL];
+// const corsOptions = {
+//   credentials: true,
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
   
-};
+// };
 
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 
 app.use(express.json());
